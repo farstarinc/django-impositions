e05c255abf38df0f60617ba29e3c3f953a021991
Ben Davis <bendavis78@gmail.com>
2012-04-30 15:15:46 -0500

    converted to pip-installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-impositions', 
+    version='0.1',
+    author='Katy Lavallee',
+    author_email='katylava@gmail.com',
+    url='http://github.com/katylava/django-impositions',
+    description='',
+    keywords='',
+    packages = find_packages(),
+    include_package_data = True
+)
